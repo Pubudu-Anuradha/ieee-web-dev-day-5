9900afbb7d2c56b054806cbf152a435c47d7c470
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2021-07-20 02:17:28 +0530

    added using state in register.js

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Card, Container, Form, Button, Row, Col } from "react-bootstrap";
 import "./register.css";
 
 export default function Register() {
+	const [email, setemail] = useState("");
+	const [password, setpassword] = useState("");
+	const [confpassword, setconfpassword] = useState("");
+
+	//componentDidMount
+	useEffect(() => {
+		console.log("The function useEffect executes");
+	}, []);
+
+	//componentDidUpdate
+	useEffect(() => {
+		console.log("Executed when email is updated");
+		console.log(email);
+	}, [email]);
+
+	//componentWillUnmount
+	useEffect(() => {
+		return () => {
+			console.log("Component unmounts");
+		};
+	}, []);
+
 	return (
 		<Container className="container" fluid="md">
 			<Row>
@@ -14,16 +36,16 @@ export default function Register() {
 							<Form>
 								<Form.Group className="mb-3" controlId="formBasicEmail">
 									<Form.Label>Email address</Form.Label>
-									<Form.Control type="email" placeholder="Enter email" />
+									<Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => {setemail(e.target.value)} }/>
 									<Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
 								</Form.Group>
 								<Form.Group className="mb-3" controlId="formBasicPassword">
 									<Form.Label>Password</Form.Label>
-									<Form.Control type="password" placeholder="Password" />
+									<Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setpassword(e.target.value)}/>
 								</Form.Group>
 								<Form.Group className="mb-3" controlId="formBasicPassword">
 									<Form.Label>Confirm Password</Form.Label>
-									<Form.Control type="password" placeholder="Password" />
+									<Form.Control type="password" placeholder="Password" value={confpassword} onChange={(e) => setconfpassword(e.target.value)}/>
 								</Form.Group>
 								<Link to="/">
 									<Form.Text className="text-muted">Already have an account?</Form.Text>
